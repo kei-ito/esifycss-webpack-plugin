d63139e71e684a6a74561bfbded575af8538410c
Kei Ito <kei.itof@gmail.com>
2021-09-11 16:34:53 +0900

    refactor: add fileExistsAt

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-/* eslint-disable class-methods-use-this */
 import * as fs from 'fs';
 import * as path from 'path';
 import type * as webpack from 'webpack';
 
 const pluginName = 'EsifyCSSWebpackPlugin';
+const fileExistsAt = async (filePath: string): Promise<boolean> => {
+    const stat = await fs.promises.stat(filePath).catch(() => null);
+    return Boolean(stat && stat.isFile());
+};
 
 export class EsifyCSSWebpackPlugin {
 
+    // eslint-disable-next-line class-methods-use-this
     public apply(compiler: webpack.Compiler) {
         compiler.hooks.normalModuleFactory.tap(pluginName, (normalModuleFactory) => {
             normalModuleFactory.hooks.resolve.tapPromise(pluginName, async (request) => {
@@ -15,9 +19,7 @@ export class EsifyCSSWebpackPlugin {
                 }
                 for (const extension of ['.js', '.ts']) {
                     const extended = `${request.request}${extension}`;
-                    const absolutePath = path.join(request.context, extended);
-                    const stat = await fs.promises.stat(absolutePath).catch(() => null);
-                    if (stat && stat.isFile()) {
+                    if (await fileExistsAt(path.join(request.context, extended))) {
                         // eslint-disable-next-line require-atomic-updates
                         request.request = extended;
                         return;

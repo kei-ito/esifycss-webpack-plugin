fe9770c74501e0f17266c95bb58a0a8d16fa9481
Kei Ito <kei.itof@gmail.com>
2021-09-11 17:47:02 +0900

    fix: input may have resolve hook

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,54 @@ import * as fs from 'fs';
 import * as path from 'path';
 import type * as webpack from 'webpack';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type CallbackParameter<T> = T extends (O: any, C: (arg: infer Arg) => any) => any ? Arg : never;
+type NormalModuleFactory = CallbackParameter<webpack.Compiler['hooks']['normalModuleFactory']['tap']>;
+type ResolveData = CallbackParameter<NormalModuleFactory['hooks']['resolve']['tapPromise']>;
+interface ObjectWithHooks {
+    hooks: Record<string, unknown>,
+}
+
 const pluginName = 'EsifyCSSWebpackPlugin';
+
 const fileExistsAt = async (filePath: string): Promise<boolean> => {
     const stat = await fs.promises.stat(filePath).catch(() => null);
     return Boolean(stat && stat.isFile());
 };
 
+const isCompilerLike = (
+    input: ObjectWithHooks,
+): input is Pick<webpack.Compiler, 'hooks'> => 'normalModuleFactory' in input.hooks;
+
+const isNormalModuleFactoryLike = (
+    input: ObjectWithHooks,
+): input is Pick<NormalModuleFactory, 'hooks'> => 'resolve' in input.hooks;
+
+const resolveModuleCss = async (resolveData: ResolveData) => {
+    if (!resolveData.request.endsWith('.module.css')) {
+        return;
+    }
+    for (const extension of ['.js', '.ts']) {
+        const extended = `${resolveData.request}${extension}`;
+        if (await fileExistsAt(path.join(resolveData.context, extended))) {
+            // eslint-disable-next-line require-atomic-updates
+            resolveData.request = extended;
+            return;
+        }
+    }
+};
+
 export class EsifyCSSWebpackPlugin {
 
     // eslint-disable-next-line class-methods-use-this
-    public apply(compiler: webpack.Compiler) {
-        compiler.hooks.normalModuleFactory.tap(pluginName, (normalModuleFactory) => {
-            normalModuleFactory.hooks.resolve.tapPromise(pluginName, async (request) => {
-                if (!request.request.endsWith('.module.css')) {
-                    return;
-                }
-                for (const extension of ['.js', '.ts']) {
-                    const extended = `${request.request}${extension}`;
-                    if (await fileExistsAt(path.join(request.context, extended))) {
-                        // eslint-disable-next-line require-atomic-updates
-                        request.request = extended;
-                        return;
-                    }
-                }
+    public apply(input: ObjectWithHooks) {
+        if (isNormalModuleFactoryLike(input)) {
+            input.hooks.resolve.tapPromise(pluginName, resolveModuleCss);
+        } else if (isCompilerLike(input)) {
+            input.hooks.normalModuleFactory.tap(pluginName, (normalModuleFactory) => {
+                normalModuleFactory.hooks.resolve.tapPromise(pluginName, resolveModuleCss);
             });
-        });
+        }
     }
 
 }
